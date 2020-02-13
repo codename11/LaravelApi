381642beeecdd88c081cdb776d78756294b2ad6b
Veljko <veljkos82@gmail.com>
2020-02-13 23:01:23 +0100

    Added some comments.

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -11,6 +11,9 @@ class ArticlesTableSeeder extends Seeder
      */
     public function run()
     {
+        /*Kreira 30 unosa. U ArticleFactory.php se navode parametri tipa, 
+        koliko ce biti duzina nekog polja unosa i sl.. 
+        U DatabaseSeeder.php se vrsi pozivanje ovog seeder-a.*/
         factory(App\Article::class, 30)->create();
     }
 }
